161b4ab2aeedf0ce33ea6264df91a9dbad8da3e8
Tri <triphan921@gmail.com>
2021-02-22 16:03:39 -0800

    commit tree 7be4202eb80f311c7191787454c4fc597b6ee6f7

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+// function multiply() {
+//     return 62 * 1;
+// }
 
-// Write your code here
+// function random(arr) {
+
+// }
+
+// function mod(num1, num2) {
+//     return num1 % num2;
+// }
+
+// function max(arr) {
+
+// }
+
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,5,20);
